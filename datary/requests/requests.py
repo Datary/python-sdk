dc197acb40b8852deb816d23ab6d11c1b5e11d6b
mmoraleda <m.moraleda@datary.io>
2018-03-07 11:22:44 +0100

    add timesleep to requests when datary returns 429 http code in response

--- datary/requests/requests.py
+++ datary/requests/requests.py
@@ -2,9 +2,10 @@
 """
 Datary sdk Requests File
 """
-from requests import RequestException
+import time
 import requests
 import structlog
+from requests import RequestException
 
 
 logger = structlog.getLogger(__name__)
@@ -16,6 +17,7 @@ class DataryRequests(object):
     """
 
     URL_BASE = "http://api.datary.io/"
+    tries_limit = 3
     headers = {}
 
     def __init__(self, **kwargs):
@@ -25,8 +27,9 @@ class DataryRequests(object):
         super(DataryRequests, self).__init__()
         self.headers = {"Content-Type": "application/x-www-form-urlencoded"}
         self.headers.update(kwargs.get('headers', {}))
+        self.tries_limit = kwargs.get('tries_limit', 3)
 
-    def request(self, url, http_method, **kwargs):
+    def request(self, url, http_method, tries=0, **kwargs):
         """
         Sends request to Datary passing config through arguments.
 
@@ -72,10 +75,40 @@ class DataryRequests(object):
 
             # Check for correct request status code.
             if 199 < content.status_code < 300:
+                tries = 0
                 return content
+
+            # Check if must wait x seconds returning 429 http code.
+            elif content.status_code == 429:
+
+                # TODO: RETRIEVE TIMESLEEP FROM RETURNED ERR MSG
+                time_sleep = 2
+
+                msg = "Fail Request to datary ({}) - Need to wait {} seconds"
+                logger.warning(
+                    msg.format(content.status_code, time_sleep),
+                    url=url, http_method=http_method,
+                    code=content.status_code,
+                    text=content.text,
+                    # kwargs=kwargs,
+                )
+                time.sleep(time_sleep)
+                tries += 1
+
+                if tries >= self.tries_limit:
+                    logger.error(
+                        "Request Tries Limit Exceeded!!",
+                        url=url,
+                        http_method=http_method,
+                        # requests_args=kwargs,
+                    )
+
+                else:
+                    return self.request(url, http_method, tries, **kwargs)
+
             else:
                 msg = "Fail Request to datary done with code {}"
-
+                tries = 0
                 logger.error(
                     msg.format(content.status_code),
                     url=url, http_method=http_method,
@@ -86,6 +119,7 @@ class DataryRequests(object):
 
         # Request Exception
         except RequestException as ex:
+            tries = 0
             logger.error(
                 "Fail request to Datary - {}".format(ex),
                 url=url,
--- datary/requests/test/test_requests.py
+++ datary/requests/test/test_requests.py
@@ -15,7 +15,8 @@ class DataryRequestsTestCase(DataryTestCase):
     """
 
     @mock.patch('datary.requests.requests.requests')
-    def test_request(self, mock_requests):
+    @mock.patch('datary.requests.requests.time')
+    def test_request(self, mock_time, mock_requests):
         """
         Test get_request
         =============   =============   =======================================
@@ -61,10 +62,41 @@ class DataryRequestsTestCase(DataryTestCase):
         result5 = self.datary.request(self.url, 'GET')
         self.assertEqual(result5, None)
 
+        # test status code 429
+        responses_429_test = [
+            MockRequestResponse(
+                "Request limit exceeded, must wait for XX seconds",
+                status_code=429),
+            MockRequestResponse(
+                "Request limit exceeded, must wait for XX seconds",
+                status_code=429),
+            MockRequestResponse(
+                "Request limit exceeded, must wait for XX seconds",
+                status_code=429),
+            MockRequestResponse(
+                "Everything OK",
+                status_code=200)
+            ]
+
+        mock_requests.get.side_effect = iter(responses_429_test)
+
+        result7 = self.datary.request(self.url, 'GET')
+        self.assertEqual(result7, None)
+
+        # upgrade tries limit
+        self.datary.tries_limit = 4
+        mock_requests.get.side_effect = iter(responses_429_test)
+
+        result8 = self.datary.request(self.url, 'GET')
+        self.assertEqual(result8.text, 'Everything OK')
+
+        self.assertEqual(mock_time.sleep.call_count, 3+3)
+
         mock_requests.get.side_effect = requests.RequestException('err')
         result6 = self.datary.request(self.url, 'GET')
         self.assertEqual(result6, None)
 
-        self.assertEqual(mock_requests.get.call_count, 3)
+        self.assertEqual(mock_requests.get.call_count, 10)
         self.assertEqual(mock_requests.post.call_count, 1)
+        self.assertEqual(mock_requests.put.call_count, 1)
         self.assertEqual(mock_requests.delete.call_count, 1)
